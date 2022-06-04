04873aaec8315dd84a0b3cf5dfaa9adaa9003f67
MohamedAlabasy <Eng.mohamed.Alabasy@gmail.com>
2022-06-04 02:03:31 +0200

    add dotenv

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
 const express = require('express');
+require('dotenv').config();
+console.log(process.env.PORT);
 
-
-const PORT = 8090;
+const PORT = process.env.PORT || 5555;
 const app = express();
 
 
-app.listen(process.env.PORT || PORT, () => {
-    console.log(`App Run at http://localhost:${PORT}`);
+app.listen(PORT, () => {
+    console.log(`App Run at http://${process.env.HOST}:${PORT}`);
 });
+
+
+// middleware not Found
+app.use((request, response, next) => {
+    response.status(404).json({
+        status: 0,
+        message: 'Not Found'
+    })
+})
+
+// middleware error
+app.use((error, request, response, next) => {
+    let status = error.status || 500;
+    response.status(status).json({
+        status: 0,
+        error: error + ''
+    })
+})
