8ac135710a4604c235f785b05f36e5f1cbede457
MohamedAlabasy <Eng.mohamed.Alabasy@gmail.com>
2022-06-07 21:01:00 +0200

    add categories schema

---
+++ Models/CategoriesSchema.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const { AutoIncrementID } = require('@typegoose/auto-increment');
+
+const schema = new mongoose.Schema({
+    _id: Number,
+    name: { type: String, required: true },
+}, { timestamps: true });
+schema.plugin(AutoIncrementID, [{ field: '_id' }]);
+
+module.exports = mongoose.model('categories', schema);
