5461bddb6b45b864b3bbd0d0fa0f95fd0de81f65
MohamedAlabasy <Eng.mohamed.Alabasy@gmail.com>
2022-06-07 21:24:48 +0200

    finish Authors

--- Models/AuthorsSchema.js
+++ Models/AuthorsSchema.js
@@ -3,6 +3,8 @@ const { AutoIncrementID } = require('@typegoose/auto-increment');
 
 const schema = new mongoose.Schema({
     _id: Number,
+    first_name: { type: String, required: true, trim: true, },
+    last_name: { type: String, required: true, trim: true },
     image: { type: String, required: true },
     gender: { type: String, enum: ['male', 'female'], default: 'male' }
 }, { timestamps: true });
