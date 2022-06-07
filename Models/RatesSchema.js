40485c97b26d27954fc2b10471fb635c500451d9
MohamedAlabasy <Eng.mohamed.Alabasy@gmail.com>
2022-06-07 21:38:28 +0200

    finish rates schema

--- Models/RatesSchema.js
+++ Models/RatesSchema.js
@@ -3,6 +3,9 @@ const { AutoIncrementID } = require('@typegoose/auto-increment');
 
 const schema = new mongoose.Schema({
     _id: Number,
+    rate: { type: String, required: true },
+    book: { type: String, ref: 'books' },
+    user: { type: String, ref: 'users' },
 }, { timestamps: true });
 
 schema.plugin(AutoIncrementID, [{ field: '_id' }]);
